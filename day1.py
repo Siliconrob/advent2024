1cddaad2f1d85b89a2ce5657bfb0f8d998a72293
Robin Michael <Siliconrob@siliconheaven.net>
2024-12-01 00:15:03 -0600

    Part 2 done

--- day1.py
+++ day1.py
@@ -1,30 +1,41 @@
-import itertools
-from aocd import get_data
-from aocd import submit
+from collections import Counter
+from typing import Tuple
 from aocd.models import Puzzle
 from icecream import ic
 from parse import parse
 
-def part1(input_data: []):
+
+def read_lists(lines_to_parse: list[str]) -> Tuple[list[int], list[int]]:
     list1 = []
     list2 = []
 
-    for input_line in input_data:
+    for input_line in lines_to_parse:
         number1, spacer, number2 = parse('{:d}{:s}{:d}', input_line)
         list1.append(number1)
         list2.append(number2)
     list1.sort()
     list2.sort()
+    return list1, list2
 
+
+def part1_solve(list1: list[int], list2: list[int]) -> int:
     diffs = []
     for index in range(len(list1)):
         list1_number = list1[index]
         list2_number = list2[index]
         diff = abs(list1_number - list2_number)
         diffs.append(diff)
-    part1 = sum(diffs)
-    ic(part1)
-    return part1
+    return sum(diffs)
+
+
+def part2_solve(list1: list[int], list2: list[int]) -> int:
+    scores = []
+    list2_counts = Counter(list2)
+    for index in range(len(list1)):
+        list1_number = list1[index]
+        matches = list2_counts.get(list1[index])
+        scores.append(0 if matches is None else matches * list1_number)
+    return sum(scores)
 
 
 def main() -> None:
@@ -38,7 +49,9 @@ def main() -> None:
     #     '3   9',
     #     '3   3'
     # ]
-    puzzle.answer_a = part1(data)
+    list1, list2 = read_lists(data)
+    puzzle.answer_a = ic(part1_solve(list1, list2))
+    puzzle.answer_b = ic(part2_solve(list1, list2))
 
 
 if __name__ == '__main__':

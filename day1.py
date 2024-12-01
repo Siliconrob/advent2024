c0c99315dae417351000ed5a28f10121c34eb570
Robin Michael <Siliconrob@siliconheaven.net>
2024-11-30 23:59:18 -0600

    Part 1 done

--- day1.py
+++ day1.py
@@ -3,82 +3,43 @@ from aocd import get_data
 from aocd import submit
 from aocd.models import Puzzle
 from icecream import ic
+from parse import parse
+
+def part1(input_data: []):
+    list1 = []
+    list2 = []
+
+    for input_line in input_data:
+        number1, spacer, number2 = parse('{:d}{:s}{:d}', input_line)
+        list1.append(number1)
+        list2.append(number2)
+    list1.sort()
+    list2.sort()
+
+    diffs = []
+    for index in range(len(list1)):
+        list1_number = list1[index]
+        list2_number = list2[index]
+        diff = abs(list1_number - list2_number)
+        diffs.append(diff)
+    part1 = sum(diffs)
+    ic(part1)
+    return part1
 
 
-def extract_number_portion(input_line: str, text_numbers: list[str]) -> int:
-    extracted_number = None
-    number_positions = {}
-    for current_index, current_character in enumerate(input_line):
-        if current_character.isdigit():
-            extracted_number = current_character
-            number_positions[current_index] = int(current_character)
-            break
-    if extracted_number is None or 2 < current_index < len(input_line):
-        if extracted_number is None:
-            current_index = len(input_line)
-        partial_line = input_line[:current_index]
-        if partial_line.isalpha():
-            for text_number in text_numbers:
-                found_index = partial_line.find(text_number)
-                if found_index > -1:
-                    number_positions[found_index] = text_numbers.index(text_number) + 1
-            number_keys = list(number_positions.keys())
-            number_keys.sort()
-            extracted_number = number_positions[number_keys[0]]
-        else:
-            for text_number in text_numbers:
-                if text_number in partial_line:
-                    extracted_number = text_numbers.index(text_number) + 1
-                    break
-    return extracted_number
-
-
-def get_part2_extracted_number(input_line: str) -> int:
-    valid_text_numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    first_digit = extract_number_portion(input_line, valid_text_numbers)
-    reversed_text_numbers = []
-    for text_number in valid_text_numbers:
-        reversed_text_numbers.append(text_number[::-1])
-    reversed_input_line = input_line[::-1]
-    last_digit = extract_number_portion(reversed_input_line, reversed_text_numbers)
-    return int(f'{first_digit}{last_digit}')
-
-
-def get_part1_extracted_number(input_line: str) -> int:
-    first_digit = None
-    last_digit = None
-    for current_character in input_line:
-        if current_character.isdigit():
-            first_digit = current_character
-            break
-    for current_character in input_line[::-1]:
-        if current_character.isdigit():
-            last_digit = current_character
-            break
-    return int(f'{first_digit}{last_digit}')
-
-def main():
-    # data = get_data(day=1, year=2023).splitlines()
-    data = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
-    data_part2 = ['two1nine', 'eightwothree', 'abcone2threexyz', 'xtwone3four', '4nineeightseven2', 'zoneight234',
-                  '7pqrstsixteen']
-
-    extracted_numbers_part1 = []
-    extracted_numbers_part2 = []
-    for current_line in data:
-        extracted_numbers_part1.append(get_part1_extracted_number(current_line))
-    print(f'Part 1 {sum(extracted_numbers_part1)}')
-
-    for current_line in data_part2:
-        extracted_numbers_part2.append(get_part2_extracted_number(current_line))
-    print(f'Part 2 {sum(extracted_numbers_part2)}')
+def main() -> None:
+    puzzle = Puzzle(year=2024, day=1)
+    data = puzzle.input_data.splitlines()
+    # data = [
+    #     '3   4',
+    #     '4   3',
+    #     '2   5',
+    #     '1   3',
+    #     '3   9',
+    #     '3   3'
+    # ]
+    puzzle.answer_a = part1(data)
 
 
 if __name__ == '__main__':
-    puzzle = Puzzle(year=2023, day=1)
-
-    ic(puzzle.input_data)
-    ic(puzzle.answer_a)
-    ic(puzzle.answer_b)
-
-    # main()
+    main()

d28c707de7fbbb5cc11a55704e278604a66f4341
Robin Michael <Siliconrob@siliconheaven.net>
2024-12-02 23:12:36 -0600

    Day 3 Part 1 done

--- day3.py
+++ day3.py
@@ -4,7 +4,7 @@ from typing import Tuple
 from aocd.models import Puzzle
 from icecream import ic
 from parse import parse
-
+import re
 
 def parse_line(input_line: str) -> list[int]:
     return [int(x) for x in input_line.split(" ")]
@@ -15,6 +15,12 @@ def get_pair_diffs(computed_pairs, is_decreasing) -> list[int]:
     for pair in computed_pairs:
         diffs.append(pair[0] - pair[1] if is_decreasing else pair[1] - pair[0])
     return diffs
+
+
+def report_details(report_values: list[int]) -> dict:
+    is_decreasing = report_values[0] > report_values[-1]
+    pair_diffs = ic(get_pair_diffs(itertools.pairwise(report_values), is_decreasing))
+    return report_error_counts(pair_diffs)
 
 
 def report_error_counts(diffs: list[int]) -> list[int]:
@@ -31,55 +37,50 @@ def report_error_counts(diffs: list[int]) -> list[int]:
     return errors
 
 
-def part1_solve(input_lines: list[str]) -> int:
-    safe_reports = 0
-    for line in input_lines:
-        report_values = parse_line(line)
-        current_errors = report_details(report_values)
-        safe_reports += 1 if len(current_errors) == 0 else 0
-    return safe_reports
+def part1_solve(input_data: str) -> int:
+    line_sum = []
+    match_pattern = r"mul\(\d+,\d+\)"
+    instructions = re.findall(match_pattern, input_data)
+    for instruction in instructions:
+        number1, number2 = parse("mul({:d},{:d})", instruction)
+        line_sum.append(number1 * number2)
+    return sum(line_sum)
 
 
-def part2_solve(input_lines: list[str]) -> int:
-    safe_reports = 0
-    for line in input_lines:
-        report_values = parse_line(line)
-        current_errors = report_details(report_values)
-        safe_reports += 1 if len(current_errors) == 0 else 0
-        continue
-
-        pair_issue_index = current_errors.pop()
-        report_values.pop(pair_issue_index)
-        current_errors = report_details(report_values)
-        safe_reports += 1 if len(current_errors) == 0 else 0
-        continue
-
-        report_values = parse_line(line)
-        report_values.pop(pair_issue_index + 1)
-        current_errors = report_details(report_values)
-        safe_reports += 1 if len(current_errors) == 0 else 0
-
-    return safe_reports
-
-
-def report_details(report_values: list[int]) -> dict:
-    is_decreasing = report_values[0] > report_values[-1]
-    pair_diffs = ic(get_pair_diffs(itertools.pairwise(report_values), is_decreasing))
-    return report_error_counts(pair_diffs)
+# def part2_solve(input_lines: list[str]) -> int:
+#     safe_reports = 0
+#     for line in input_lines:
+#         report_values = parse_line(line)
+#         current_errors = report_details(report_values)
+#         safe_reports += 1 if len(current_errors) == 0 else 0
+#         continue
+#
+#         pair_issue_index = current_errors.pop()
+#         report_values.pop(pair_issue_index)
+#         current_errors = report_details(report_values)
+#         safe_reports += 1 if len(current_errors) == 0 else 0
+#         continue
+#
+#         report_values = parse_line(line)
+#         report_values.pop(pair_issue_index + 1)
+#         current_errors = report_details(report_values)
+#         safe_reports += 1 if len(current_errors) == 0 else 0
+#
+#     return safe_reports
 
 
 def main() -> None:
     puzzle = Puzzle(year=2024, day=3)
     example = puzzle.examples.pop()
 
-    input_lines = puzzle.input_data.splitlines()
-    example_lines = example.input_data.splitlines()
+    input_lines = puzzle.input_data
+    example_lines = example.input_data
 
-    if int(example.answer_a) == ic(part1_solve(example_lines)):
+    if 161 == ic(part1_solve(example_lines)):
         puzzle.answer_a = ic(part1_solve(input_lines))
 
-    if int(example.answer_b) == ic(part2_solve(example_lines)):
-        puzzle.answer_b = ic(part2_solve(input_lines))
+    # if int(example.answer_b) == ic(part2_solve(example_lines)):
+    #     puzzle.answer_b = ic(part2_solve(input_lines))
 
 
 if __name__ == '__main__':

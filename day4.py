983868582a57a03bc571a89ffd5e27d0bcaea351
Robin Michael <Siliconrob@siliconheaven.net>
2024-12-04 00:02:10 -0600

    Day 4 part 1

--- day4.py
+++ day4.py
@@ -7,12 +7,90 @@ from parse import parse
 import re
 from more_itertools import peekable
 
-multiply_match_pattern: str = r"mul\(\d+,\d+\)"
+
+def left_to_right_diagonals(input_data: list[str]) -> list[str]:
+    lines = []
+    size = len(input_data)
+    for col in range(size):
+        diagonal = []
+        x, y = 0, col
+        while x < size and y < size:
+            diagonal.append(input_data[x][y])
+            x += 1
+            y += 1
+        lines.append("".join(diagonal))
+    for row in range(1, size):
+        diagonal = []
+        x, y = row, 0
+        while x < size and y < size:
+            diagonal.append(input_data[x][y])
+            x += 1
+            y += 1
+        lines.append("".join(diagonal))
+    return lines
 
 
-def part1_solve(input_data: str) -> int:
-    instructions = re.findall(multiply_match_pattern, input_data)
-    return instructions_total(instructions)
+def right_to_left_diagonals(input_data: list[str]) -> list[str]:
+    lines = []
+    size = len(input_data)
+    for col in range(size):
+        diagonal = []
+        x, y = 0, col
+        while x < size and y >= 0:
+            diagonal.append(input_data[x][y])
+            x += 1
+            y -= 1
+        lines.append("".join(diagonal))
+    for row in range(1, size):
+        diagonal = []
+        x, y = row, size - 1
+        while x < size and y >= 0:
+            diagonal.append(input_data[x][y])
+            x += 1
+            y -= 1
+        lines.append("".join(diagonal))
+    return lines
+
+
+def find_pattern(input_line: str, search: str) -> int:
+    result = sum([
+        len(re.findall(search, input_line, flags=re.IGNORECASE)),
+        len(re.findall(search, input_line[::-1], flags=re.IGNORECASE))
+    ])
+    return result
+
+
+def part1_solve(input_data: list[str]) -> int:
+    match_pattern = "xmas"
+    total = 0
+    total += sum([find_pattern(line, match_pattern) for line in input_data])
+    total += sum([find_pattern(line, match_pattern) for line in ["".join(column) for column in zip(*input_data)]])
+    total += sum([find_pattern(line, match_pattern) for line in left_to_right_diagonals(input_data)])
+    total += sum([find_pattern(line, match_pattern) for line in right_to_left_diagonals(input_data)])
+    return total
+
+    #     horizontal_matches.append(len(re.findall(match_pattern, line, flags=re.IGNORECASE)))
+    #     horizontal_matches.append(len(re.findall(match_pattern, line[::-1], flags=re.IGNORECASE)))
+    # total += sum(horizontal_matches)
+
+    # for line in ["".join(column) for column in zip(*input_data)]:
+    #     vertical_matches.append(len(re.findall(match_pattern, line, flags=re.IGNORECASE)))
+    #     vertical_matches.append(len(re.findall(match_pattern, line[::-1], flags=re.IGNORECASE)))
+    # total += sum(vertical_matches)
+    #
+    # for line in left_to_right_diagonals(input_data):
+    #     left_to_right.append(len(re.findall(match_pattern, line, flags=re.IGNORECASE)))
+    #     left_to_right.append(len(re.findall(match_pattern, line[::-1], flags=re.IGNORECASE)))
+    # total += sum(left_to_right)
+    #
+    # for line in right_to_left_diagonals(input_data):
+    #     right_to_left.append(len(re.findall(match_pattern, line, flags=re.IGNORECASE)))
+    #     right_to_left.append(len(re.findall(match_pattern, line[::-1], flags=re.IGNORECASE)))
+    # total += sum(right_to_left)
+    #
+    # return total
+    # instructions = re.findall(multiply_match_pattern, input_data)
+    # return instructions_total(instructions)
 
 
 def instructions_total(instructions: list[str]) -> int:
@@ -40,15 +118,23 @@ def main() -> None:
     puzzle = Puzzle(year=2024, day=4)
     example = puzzle.examples.pop()
 
-    input_lines = puzzle.input_data
-    example_line_part1 = example.input_data
-    example_line_part2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    input_lines = puzzle.input_data.splitlines()
+    example_line_part1 = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX""".splitlines()
 
-    if 161 == ic(part1_solve(example_line_part1)):
+    if 18 == ic(part1_solve(example_line_part1)):
         puzzle.answer_a = ic(part1_solve(input_lines))
 
-    if 48 == ic(part2_solve(example_line_part2)):
-        puzzle.answer_b = ic(part2_solve(input_lines))
+    # if 48 == ic(part2_solve(example_line_part2)):
+    #     puzzle.answer_b = ic(part2_solve(input_lines))
 
 
 if __name__ == '__main__':

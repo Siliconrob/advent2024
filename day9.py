c6ff07deb5db6d7b1a8ae8260026b4656939e375
Robin Michael <Siliconrob@siliconheaven.net>
2024-12-09 00:31:17 -0600

    Day 9 Part 1 start

--- day9.py
+++ day9.py
@@ -1,11 +1,14 @@
 import copy
 import itertools
+from _pyrepl.completing_reader import complete
 from collections import deque
 from collections.abc import Callable
 from dataclasses import dataclass, field
 from functools import reduce
 from aocd.models import Puzzle
 from icecream import ic
+from more_itertools import peekable
+from numpy.ma.core import empty
 from sympy import symbols, Function, Eq, Piecewise
 from sympy import solve
 
@@ -14,100 +17,67 @@ from sympy.abc import x, y, z, a, b, c, d, e
 
 
 @dataclass
-class Equation:
-    result: int = 0
-    inputs: list[int] = field(default_factory=list)
+class FileSystem:
+    files: dict = field(default_factory=dict)
+    emtpy_blocks: list[int] = field(default_factory=list)
 
 
-def general_solve(input_data: list[str], part_b: bool = False) -> int:
-    max_x, max_y = len(input_data), len(input_data[0])
-    antenna_groups = find_antennas(input_data)
-    anti_nodes = []
-    for (antenna_type, antenna_group) in antenna_groups.items():
-        for antenna_pair in itertools.combinations(antenna_group, 2):
-            location1, location2 = antenna_pair[0], antenna_pair[1]
-
-            y_diff = location1[0] - location2[0]
-            x_diff = location1[1] - location2[1]
-            new_x_diff = 0
-            new_y_diff = 0
-            while True:
-                new_x_diff += y_diff
-                new_y_diff += x_diff
-                poss_points = set([
-                    (location1[0] - new_y_diff, location1[1] - new_x_diff),
-                    (location1[0] + new_y_diff, location1[1] + new_x_diff),
-                    (location2[0] - new_y_diff, location2[1] - new_x_diff),
-                    (location2[0] + new_y_diff, location2[1] + new_x_diff)
-                ])
-                current_points = [location1, location2]
-                poss_points = poss_points - set(current_points)
-
-                invalid_points = 0
-                for point in poss_points:
-                    point_y, point_x = point[0], point[1]
-                    if -1 < point_y < max_y and -1 < point_x < max_x:
-                        anti_nodes.append(point)
-                    else:
-                        invalid_points += 1
-                if invalid_points == len(poss_points):
-                    break
-                if part_b is False:
-                    break
-    uniques = set(anti_nodes)
-    print_map(input_data, uniques)
-    return len(set(anti_nodes))
+def parse_input(input_line: str) -> FileSystem:
+    fs = FileSystem()
+    current_file_index = 0
+    for index in range(len(input_line)):
+        current_item = int(input_line[index])
+        if index % 2 == 0:
+            fs.files[current_file_index] = current_item
+            current_file_index += 1
+            continue
+        fs.emtpy_blocks.append(current_item)
+    return fs
 
 
-def print_map(input_data, uniques):
-    lines = []
+def move_blocks(input_fs: FileSystem) -> list[int]:
+    current_file_blocks = []
+    current_empties = peekable(input_fs.emtpy_blocks)
+    for (id, length) in input_fs.files.items():
+        file = "".join([str(z) for z in list(itertools.repeat(id, length))])
+        current_file_blocks.append(file)
+        if current_empties:
+            empties = "".join(list(itertools.repeat('.', next(current_empties))))
+            current_file_blocks.append(empties)
 
-    for y in range(len(input_data)):
-        line = []
-        for x in range(len(input_data[y])):
-            point_value = input_data[y][x]
-            if (y, x) in uniques:
-                point_value = "#"
-            line.append(point_value)
-        lines.append("".join(line))
-    ic(lines)
+    complete = list("".join(current_file_blocks))
+    while '.' in complete:
+        sizeof = len(complete)
+        location = complete.index('.')
+        complete[location] = complete[-1]
+        complete = complete[:sizeof - 1]
 
-
-def find_antennas(input_data):
-    current_node_sets = {}
-    for y in range(len(input_data)):
-        for x in range(len(input_data[y])):
-            point_value = input_data[y][x]
-            if point_value == '.':
-                continue
-            current_points = current_node_sets.get(point_value)
-            if current_points is None:
-                current_node_sets[point_value] = [(y, x)]
-                continue
-            current_points.append((y, x))
-            current_node_sets[point_value] = current_points
-    return current_node_sets
+    blocks = [int(index) for index in complete]
+    return blocks
 
 
-def part1_solve(input_data: list[str]) -> int:
-    return general_solve(input_data, False)
+def part1_solve(input_data: str) -> int:
+    current_fs = parse_input(input_data)
+    compressed_files = move_blocks(current_fs)
+    checksum = sum([index * compressed_files[index] for index in range(len(compressed_files))])
+    return checksum
 
 
 def part2_solve(input_data: list[str]) -> int:
-    return general_solve(input_data, True)
+    pass
 
 
 def main() -> None:
     puzzle = Puzzle(year=2024, day=9)
-    input_lines = puzzle.input_data.splitlines()
+    input_lines = puzzle.input_data
     example = puzzle.examples.pop()
-    example_input = example.input_data.splitlines()
+    example_input = example.input_data
 
     if int(example.answer_a) == ic(part1_solve(example_input)):
         puzzle.answer_a = ic(part1_solve(input_lines))
 
-    if 34 == ic(part2_solve(example_input)):
-        puzzle.answer_b = ic(part2_solve(input_lines))
+    # if 34 == ic(part2_solve(example_input)):
+    #     puzzle.answer_b = ic(part2_solve(input_lines))
 
 
 if __name__ == '__main__':

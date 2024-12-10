ebafb815987688d0d60b850cbb910db86a97bdc1
Robin Michael <Siliconrob@siliconheaven.net>
2024-12-10 00:20:02 -0600

    Cleanup Day 10

--- day10.py
+++ day10.py
@@ -40,7 +40,7 @@ def parse_inputs(input_data):
     return bounds, matrix, start_points
 
 
-def part1_solve(input_data: str) -> int:
+def general_solve(input_data: str, part_b: bool = False) -> int:
     bounds, matrix, start_points = parse_inputs(input_data)
     all_paths = {}
     for y, x in start_points:
@@ -57,51 +57,37 @@ def part1_solve(input_data: str) -> int:
             current_value = matrix[current_y][current_x]
             if current_value == 9:
                 id = f"({current_y},{current_x})"
-                ending = paths.get(id, None)
-                if ending is None:
-                    paths[id] = current_value
+                if part_b:
+                    current_paths = paths.get(id, None)
+                    if current_paths is None:
+                        paths[id] = [current_stack]
+                    else:
+                        current_paths.append(current_stack)
+                else:
+                    ending = paths.get(id, None)
+                    if ending is None:
+                        paths[id] = current_value
             for pos_y, pos_x in neighbors:
                 if -1 < pos_y < bounds[0] and -1 < pos_x < bounds[1]:
                     if current_value + 1 == matrix[pos_y][pos_x]:
                         current_stack.append((pos_y, pos_x))
         all_paths[f"({y},{x})"] = paths
-    total_paths = ic(sum([len(paths) for paths in all_paths.values()]))
+    if part_b:
+        total_paths = 0
+        for paths in all_paths.values():
+            for current_path in paths.values():
+                total_paths += len(current_path)
+    else:
+        total_paths = ic(sum([len(paths) for paths in all_paths.values()]))
     return total_paths
+
+
+def part1_solve(input_data: str) -> int:
+    return general_solve(input_data, False)
 
 
 def part2_solve(input_data: str) -> int:
-    bounds, matrix, start_points = parse_inputs(input_data)
-    all_paths = {}
-    for y, x in start_points:
-        paths = {}
-        current_stack = deque([(y, x)])
-        while current_stack:
-            current_y, current_x = current_stack.pop()
-            neighbors = [
-                (current_y - 1, current_x),
-                (current_y + 1, current_x),
-                (current_y, current_x - 1),
-                (current_y, current_x + 1),
-            ]
-            current_value = matrix[current_y][current_x]
-            if current_value == 9:
-                id = f"({current_y},{current_x})"
-                current_paths = paths.get(id, None)
-                if current_paths is None:
-                    paths[id] = [current_stack]
-                else:
-                    current_paths.append(current_stack)
-            for pos_y, pos_x in neighbors:
-                if -1 < pos_y < bounds[0] and -1 < pos_x < bounds[1]:
-                    if current_value + 1 == matrix[pos_y][pos_x]:
-                        current_stack.append((pos_y, pos_x))
-        all_paths[f"({y},{x})"] = paths
-
-    total_paths = 0
-    for paths in all_paths.values():
-        for current_path in paths.values():
-            total_paths += len(current_path)
-    return total_paths
+    return general_solve(input_data, True)
 
 
 def main() -> None:

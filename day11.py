c450d9a39a04929f5a828e651fffa25d220c765c
Robin Michael <Siliconrob@siliconheaven.net>
2024-12-11 00:59:23 -0600

    Recursive function not right yet, but is most likely way to solve

--- day11.py
+++ day11.py
@@ -1,9 +1,11 @@
 import copy
 import itertools
+import sys
 from _pyrepl.completing_reader import complete
 from collections import deque, Counter
 from collections.abc import Callable
 from dataclasses import dataclass, field
+import time
 from functools import reduce
 
 import numpy as np
@@ -48,7 +50,7 @@ def split_string(input_text: str) -> list[str]:
     return [str(int(left_half)), str(int(right_half))]
 
 
-def part1_solve(input_data: str) -> int:
+def general_solve(input_data: str, rounds: int) -> int:
     stones = input_data.split(" ")
     blink_ops = [
         lambda x: '1' if int(x) == 0 else None,
@@ -56,7 +58,7 @@ def part1_solve(input_data: str) -> int:
         lambda x: str(int(x) * 2024)
     ]
 
-    for rounds in range(25):
+    for round in range(rounds):
         new_stones = []
         for stone in stones:
             for op in blink_ops:
@@ -69,43 +71,51 @@ def part1_solve(input_data: str) -> int:
                     new_stones.append(current_stones)
                 break
         # ic(f"{round}: {new_stones}")
+        ic(f"{round}")
+        ic(f"{new_stones}")
         stones = new_stones
     return len(stones)
 
 
-def part2_solve(input_data: str) -> int:
-    bounds, matrix, start_points = parse_inputs(input_data)
-    all_paths = {}
-    for y, x in start_points:
-        paths = {}
-        current_stack = deque([(y, x)])
-        while current_stack:
-            current_y, current_x = current_stack.pop()
-            neighbors = [
-                (current_y - 1, current_x),
-                (current_y + 1, current_x),
-                (current_y, current_x - 1),
-                (current_y, current_x + 1),
-            ]
-            current_value = matrix[current_y][current_x]
-            if current_value == 9:
-                id = f"({current_y},{current_x})"
-                current_paths = paths.get(id, None)
-                if current_paths is None:
-                    paths[id] = [current_stack]
-                else:
-                    current_paths.append(current_stack)
-            for pos_y, pos_x in neighbors:
-                if -1 < pos_y < bounds[0] and -1 < pos_x < bounds[1]:
-                    if current_value + 1 == matrix[pos_y][pos_x]:
-                        current_stack.append((pos_y, pos_x))
-        all_paths[f"({y},{x})"] = paths
+def part1_solve(input_data: str) -> int:
+    return general_solve(input_data, 25)
 
-    total_paths = 0
-    for paths in all_paths.values():
-        for current_path in paths.values():
-            total_paths += len(current_path)
-    return total_paths
+
+def next_values(op_value: str) -> list[str]:
+    blink_ops = [
+        lambda x: '1' if int(x) == 0 else None,
+        lambda x: split_string(x) if len(x) % 2 == 0 else None,
+        lambda x: str(int(x) * 2024)
+    ]
+    for op in blink_ops:
+        current_stones = op(op_value)
+        if current_stones is None:
+            continue
+        if type(current_stones) is list:
+            return current_stones
+        else:
+            return [current_stones]
+
+
+def run_op(input_values: deque, round_sum: int, all_rounds_sum: int, round: int):
+    current_item = input_values.popleft()
+    if current_item is None:
+        round += 1
+        if round == 75:
+            return all_rounds_sum + round_sum
+    if current_item is None:
+        input_values.append(None)
+        return run_op(input_values, 0, all_rounds_sum + round_sum, round)
+    input_values.extend(next_values(current_item))
+    return run_op(input_values, round_sum + 1, all_rounds_sum, round)
+
+
+def part2_solve(input_data: str) -> int:
+    stones = input_data.split(" ")
+    current_queue = deque(stones)
+    current_queue.append(None)
+    all_stones = run_op(current_queue, 0, 0, 0)
+    return all_stones
 
 
 def main() -> None:
@@ -114,8 +124,14 @@ def main() -> None:
     example = puzzle.examples.pop()
     example_input = "125 17"
 
-    if int(example.answer_a) == ic(part1_solve(example_input)):
-        puzzle.answer_a = ic(part1_solve(input_lines))
+    # if int(example.answer_a) == ic(part1_solve(example_input)):
+    #     puzzle.answer_a = ic(part1_solve(input_lines))
+
+    sys.setrecursionlimit(1500000)
+
+    ic(part2_solve(example_input))
+
+    # puzzle.answer_b = ic(part2_solve(input_lines))
 
     # if 81 == ic(part2_solve(example_input)):
     #     puzzle.answer_b = ic(part2_solve(input_lines))

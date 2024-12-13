23e9b969de877c4be6f2e6b1ba0ed1c1f8ec71c9
Robin Michael <Siliconrob@siliconheaven.net>
2024-12-13 00:55:01 -0600

    Day 13 Part 2

--- day13.py
+++ day13.py
@@ -29,6 +29,13 @@ from sympy.abc import x, y, z, a, b, c, d, e
 class PrizeLocation:
     X: int = 0
     Y: int = 0
+    PrizeAdd: int = 0
+
+    def get_x(self):
+        return self.X + self.PrizeAdd
+
+    def get_y(self):
+        return self.Y + self.PrizeAdd
 
 
 @dataclass
@@ -45,8 +52,30 @@ class ClawGame:
     ButtonB: ButtonMove = field(default_factory=ButtonMove)
     Prize: PrizeLocation = field(default_factory=PrizeLocation)
 
-    def solve(self) -> Tuple[int, int]:
-        x, y = symbols('x y')
+    def solve_part2(self) -> int:
+        x = symbols('x', integer=True)
+        y = symbols('y', integer=True)
+        ax = self.ButtonA.MoveX
+        ay = self.ButtonA.MoveY
+        bx = self.ButtonB.MoveX
+        by = self.ButtonB.MoveY
+        ans1 = self.Prize.get_x()
+        ans2 = self.Prize.get_y()
+
+        equation1 = Eq(ax * x + bx * y, ans1)
+        equation2 = Eq(ay * x + by * y, ans2)
+        solution = solve((equation1, equation2), (x, y))
+
+        if solution is None or len(solution) == 0:
+            return None
+
+        x_count = solution[x]
+        y_count = solution[y]
+        return x_count * self.ButtonA.TokenCost + y_count * self.ButtonB.TokenCost
+
+    def solve_part1(self) -> int:
+        x = symbols('x', integer=True)
+        y = symbols('y', integer=True)
         ax = self.ButtonA.MoveX
         ay = self.ButtonA.MoveY
         bx = self.ButtonB.MoveX
@@ -57,12 +86,12 @@ class ClawGame:
         equation1 = Eq(ax * x + bx * y, ans1)
         equation2 = Eq(ay * x + by * y, ans2)
         solution = solve((equation1, equation2), (x, y))
+        if solution is None or len(solution) == 0:
+            return None
 
         x_count = solution[x]
         y_count = solution[y]
-        if x_count.is_Integer and y_count.is_Integer:
-            return x_count * self.ButtonA.TokenCost + y_count * self.ButtonB.TokenCost
-        return None
+        return x_count * self.ButtonA.TokenCost + y_count * self.ButtonB.TokenCost
 
 
 def parse_button(button_input: str, tokens: int) -> ButtonMove:
@@ -70,17 +99,23 @@ def parse_button(button_input: str, tokens: int) -> ButtonMove:
     return ButtonMove(button, x, y, tokens)
 
 
-def parse_game(game_inputs: list[str]) -> ClawGame:
+def parse_game(game_inputs: list[str], prize_add=0) -> ClawGame:
     buttonA = parse_button(game_inputs[0], 3)
     buttonB = parse_button(game_inputs[1], 1)
     x, y = parse("Prize: X={:d}, Y={:d}", game_inputs[2])
-    prize = PrizeLocation(x, y)
+    prize = PrizeLocation(x, y, prize_add)
     return ClawGame(buttonA, buttonB, prize)
 
 
 def part2_solve(input_data_groups: list[str]) -> int:
-    game_inputs = [line_group.splitlines() for line_group in input_data_groups]
-    pass
+    games = [parse_game(line_group.splitlines(), 10000000000000) for line_group in input_data_groups]
+    winning_games = {}
+    for game_index in range(len(games)):
+        game = games[game_index]
+        game_result = ic(game.solve_part2())
+        if game_result is not None:
+            winning_games[game_index + 1] = game_result
+    return ic(sum(winning_games.values()))
 
 
 def part1_solve(input_data_groups: list[str]) -> int:
@@ -88,7 +123,7 @@ def part1_solve(input_data_groups: list[str]) -> int:
     winning_games = {}
     for game_index in range(len(games)):
         game = games[game_index]
-        game_result = ic(game.solve())
+        game_result = ic(game.solve_part1())
         if game_result is not None:
             winning_games[game_index + 1] = game_result
     return ic(sum(winning_games.values()))
@@ -103,8 +138,8 @@ def main() -> None:
     if 480 == ic(part1_solve(example_input)):
         puzzle.answer_a = ic(part1_solve(input_lines))
 
-    # if 1206 == ic(part2_solve(example_input)):
-    #     puzzle.answer_b = ic(part2_solve(input_lines))
+    if 875318608908 == ic(part2_solve(example_input)):
+        puzzle.answer_b = ic(part2_solve(input_lines))
 
 
 if __name__ == '__main__':

1d4a8bb4a8eaf6cd4641d6177385a6106d4b6674
Robin Michael <Siliconrob@siliconheaven.net>
2024-12-14 20:07:17 -0600

    Day 14 Part 1

--- day14.py
+++ day14.py
@@ -26,117 +26,96 @@ from sympy.abc import x, y, z, a, b, c, d, e
 
 
 @dataclass
-class PrizeLocation:
-    X: int = 0
-    Y: int = 0
-    PrizeAdd: int = 0
-
-    def get_x(self):
-        return self.X + self.PrizeAdd
-
-    def get_y(self):
-        return self.Y + self.PrizeAdd
+class Grid:
+    min_x: int = 0
+    min_y: int = 0
+    max_x: int = 0
+    max_y: int = 0
 
 
 @dataclass
-class ButtonMove:
-    Name: str = None
-    MoveX: int = 0
-    MoveY: int = 0
-    TokenCost: int = 0
+class Point:
+    x: int = 0
+    y: int = 0
 
 
 @dataclass
-class ClawGame:
-    ButtonA: ButtonMove = field(default_factory=ButtonMove)
-    ButtonB: ButtonMove = field(default_factory=ButtonMove)
-    Prize: PrizeLocation = field(default_factory=PrizeLocation)
+class Move:
+    delta_x: int = 0
+    delta_y: int = 0
 
-    def solve_part2(self) -> int:
-        x = symbols('x', integer=True)
-        y = symbols('y', integer=True)
-        ax = self.ButtonA.MoveX
-        ay = self.ButtonA.MoveY
-        bx = self.ButtonB.MoveX
-        by = self.ButtonB.MoveY
-        ans1 = self.Prize.get_x()
-        ans2 = self.Prize.get_y()
-
-        equation1 = Eq(ax * x + bx * y, ans1)
-        equation2 = Eq(ay * x + by * y, ans2)
-        solution = solve((equation1, equation2), (x, y))
-
-        if solution is None or len(solution) == 0:
-            return None
-
-        x_count = solution[x]
-        y_count = solution[y]
-        return x_count * self.ButtonA.TokenCost + y_count * self.ButtonB.TokenCost
-
-    def solve_part1(self) -> int:
-        x = symbols('x', integer=True)
-        y = symbols('y', integer=True)
-        ax = self.ButtonA.MoveX
-        ay = self.ButtonA.MoveY
-        bx = self.ButtonB.MoveX
-        by = self.ButtonB.MoveY
-        ans1 = self.Prize.X
-        ans2 = self.Prize.Y
-
-        equation1 = Eq(ax * x + bx * y, ans1)
-        equation2 = Eq(ay * x + by * y, ans2)
-        solution = solve((equation1, equation2), (x, y))
-        if solution is None or len(solution) == 0:
-            return None
-
-        x_count = solution[x]
-        y_count = solution[y]
-        return x_count * self.ButtonA.TokenCost + y_count * self.ButtonB.TokenCost
+    def slope(self):
+        return self.delta_y / self.delta_x
 
 
-def parse_button(button_input: str, tokens: int) -> ButtonMove:
-    button, x, y = parse("Button {:w}: X={:d}, Y={:d}", button_input.replace("+", "=").strip())
-    return ButtonMove(button, x, y, tokens)
+@dataclass
+class Robot:
+    location: Point = field(default_factory=Point)
+    move: Move = field(default_factory=Move)
+
+    def move_next(self, the_grid: Grid):
+        next_x = (self.location.x + self.move.delta_x) % the_grid.max_x
+        next_y = (self.location.y + self.move.delta_x * self.move.slope()) % the_grid.max_y
+        self.location = Point(next_x, next_y)
 
 
-def parse_game(game_inputs: list[str], prize_add=0) -> ClawGame:
-    buttonA = parse_button(game_inputs[0], 3)
-    buttonB = parse_button(game_inputs[1], 1)
-    x, y = parse("Prize: X={:d}, Y={:d}", game_inputs[2])
-    prize = PrizeLocation(x, y, prize_add)
-    return ClawGame(buttonA, buttonB, prize)
+def parse_robot(robot_inputs: list[str]) -> Robot:
+    start_x, start_y, move_x, move_y = parse("p={:d},{:d} v={:d},{:d}", robot_inputs.strip())
+    return Robot(Point(start_x, start_y), Move(move_x, move_y))
 
 
-def part2_solve(input_data_groups: list[str]) -> int:
-    games = [parse_game(line_group.splitlines(), 10000000000000) for line_group in input_data_groups]
-    winning_games = {}
-    for game_index in range(len(games)):
-        game = games[game_index]
-        game_result = ic(game.solve_part2())
-        if game_result is not None:
-            winning_games[game_index + 1] = game_result
-    return ic(sum(winning_games.values()))
+def part2_solve(input_data: list[str]) -> int:
+    return 0
 
 
-def part1_solve(input_data_groups: list[str]) -> int:
-    games = [parse_game(line_group.splitlines()) for line_group in input_data_groups]
-    winning_games = {}
-    for game_index in range(len(games)):
-        game = games[game_index]
-        game_result = ic(game.solve_part1())
-        if game_result is not None:
-            winning_games[game_index + 1] = game_result
-    return ic(sum(winning_games.values()))
+def within_bounds(robot: Robot, grid_bounds: Grid) -> bool:
+    is_in_x = grid_bounds.min_x <= robot.location.x <= grid_bounds.max_x
+    is_in_y = grid_bounds.min_y <= robot.location.y <= grid_bounds.max_y
+    return is_in_x and is_in_y
+
+
+def part1_solve(input_data: list[str], grid: Grid, seconds: int) -> int:
+    robots = [parse_robot(x) for x in input_data]
+    second = 0
+    while second < seconds:
+        for robot in robots:
+            robot.move_next(grid)
+        second += 1
+
+    blocks = np.zeros((grid.max_y, grid.max_x))
+    for robot in robots:
+        current = blocks[int(robot.location.y), int(robot.location.x)]
+        blocks[int(robot.location.y), int(robot.location.x)] = current + 1
+
+    midpoint_x = grid.max_x // 2
+    midpoint_y = grid.max_y // 2
+
+    blocks[midpoint_y, :] = 0
+    blocks[:, midpoint_x] = 0
+
+    quadrants = {
+        0: np.sum(blocks[:midpoint_y, :midpoint_x]),
+        1: np.sum(blocks[:midpoint_y, midpoint_x:]),
+        2: np.sum(blocks[midpoint_y:, :midpoint_x]),
+        3: np.sum(blocks[midpoint_y:, midpoint_x:])
+    }
+    quad_value = ic(reduce(lambda x, y: x * y, quadrants.values()))
+    return ic(quad_value)
 
 
 def main() -> None:
     puzzle = Puzzle(year=2024, day=14)
-    input_lines = puzzle.input_data.split("\n\n")
+    input_lines = puzzle.input_data.splitlines()
+    input_grid = Grid(max_x=101, max_y=103)
     example = puzzle.examples.pop()
-    example_input = example.input_data.split("\n\n")
+    example_input = example.input_data.splitlines()
 
-    if 480 == ic(part1_solve(example_input)):
-        puzzle.answer_a = ic(part1_solve(input_lines))
+    # example_input = "p=2,4 v=2,-3\n".splitlines()
+    example_grid = Grid(max_x=11, max_y=7)
+
+    seconds = 100
+    if 12 == ic(part1_solve(example_input, example_grid, seconds)):
+        puzzle.answer_a = ic(part1_solve(input_lines, input_grid, seconds))
 
     # if 875318608908 == ic(part2_solve(example_input)):
     #     puzzle.answer_b = ic(part2_solve(input_lines))

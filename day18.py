36c40454dd8656a62b5bf629ce9f66fd09a36f5d
Robin Michael <Siliconrob@siliconheaven.net>
2024-12-18 01:25:56 -0600

    Day 18 part 2 done

--- day18.py
+++ day18.py
@@ -32,15 +32,7 @@ f = Function('f')
 from sympy.abc import x, y, z, a, b, c, d, e
 
 
-def part1_solve(input_data: str, grid_size: int, steps: int) -> int:
-    input_grid = np.zeros((grid_size, grid_size), dtype=str)
-    for coord in input_data:
-        if steps == 0:
-            break
-        x, y = parse("{:d},{:d}", coord)
-        input_grid[y, x] = '#'
-        steps -= 1
-
+def find_path(current_grid: np.ndarray) -> networkx.Graph:
     moves = [
         (0, -1),  # left
         (-1, 0),  # up
@@ -50,13 +42,14 @@ def part1_solve(input_data: str, grid_size: int, steps: int) -> int:
 
     wall = '#'
 
+    graph = nx.Graph()
     g = nx.Graph()
-    max_y_bounds = input_grid.shape[0] - 1
-    max_x_bounds = input_grid.shape[1] - 1
+    max_y_bounds = current_grid.shape[0] - 1
+    max_x_bounds = current_grid.shape[1] - 1
     start_node = f'0,0'
     end_node = f'{max_y_bounds},{max_x_bounds}'
 
-    for (y, x), input_value in np.ndenumerate(input_grid):
+    for (y, x), input_value in np.ndenumerate(current_grid):
         if input_value == wall:
             continue
         current_node = f'{y},{x}'
@@ -68,19 +61,40 @@ def part1_solve(input_data: str, grid_size: int, steps: int) -> int:
                 continue
             if next_x < 0 or next_x > max_x_bounds:
                 continue
-            next_pos_value = input_grid[next_y][next_x]
+            next_pos_value = current_grid[next_y][next_x]
             next_node = f'{next_y},{next_x}'
             if next_pos_value != wall:
                 if g.has_node(next_node) is False:
                     g.add_node(next_node, name=input_value)
                 g.add_edge(current_node, next_node)
-    path = nx.shortest_path(g, start_node, end_node)
+    return nx.shortest_path(g, start_node, end_node)
+
+
+def part1_solve(input_data: str, grid_size: int, steps: int) -> int:
+    input_grid = np.zeros((grid_size, grid_size), dtype=str)
+    for coord in input_data:
+        if steps == 0:
+            break
+        x, y = parse("{:d},{:d}", coord)
+        input_grid[y, x] = '#'
+        steps -= 1
+    path = find_path(input_grid)
     return len(path) - 1
 
 
+def part2_solve(input_data: str, grid_size: int) -> str:
+    input_grid = np.zeros((grid_size, grid_size), dtype=str)
 
-def part2_solve(input_data: str) -> int:
-    return 0
+    blocking_coord = None
+    for coord in input_data:
+        x, y = parse("{:d},{:d}", coord)
+        input_grid[y, x] = '#'
+        try:
+            path = find_path(input_grid)
+        except networkx.exception.NetworkXNoPath:
+            blocking_coord = coord
+            break
+    return blocking_coord
 
 
 def main() -> None:
@@ -89,11 +103,11 @@ def main() -> None:
     example = puzzle.examples.pop()
     example_input = example.input_data.splitlines()
 
-    if 22 == ic(part1_solve(example_input, 7, 12)):
-        puzzle.answer_a = ic(part1_solve(input_lines, 71, 1024))
+    # if 22 == ic(part1_solve(example_input, 7, 12)):
+    #     puzzle.answer_a = ic(part1_solve(input_lines, 71, 1024))
 
-    if "6,1" == ic(part2_solve(example_input)):
-        puzzle.answer_b = ic(part2_solve(input_lines))
+    if "6,1" == ic(part2_solve(example_input, 7)):
+        puzzle.answer_b = ic(part2_solve(input_lines, 71))
 
 
 if __name__ == '__main__':

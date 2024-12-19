386c6d883b65a474c56b2e00d1969f745b1e2ec3
Robin Michael <Siliconrob@siliconheaven.net>
2024-12-19 17:46:54 -0600

    Convert match to return number and cache
    Don't need to keep moving queue

--- day19.py
+++ day19.py
@@ -33,44 +33,58 @@ f = Function('f')
 from sympy.abc import x, y, z, a, b, c, d, e
 
 
-def find_matches(search_pattern: str, towel_types: list[str]):
-    for towel in sorted(towel_types, key=lambda t: len(t), reverse=True):
+# @cache
+# def find_matches(search_pattern: str, towel_types: frozenset[str]):
+#     for towel in sorted(towel_types, key=lambda t: len(t), reverse=True):
+#         if search_pattern.startswith(towel):
+#             yield towel
+
+# current_matches = find_matches(search_pattern, towel_types)
+# if current_matches is None:
+#     return 0
+# partial_matches = deque([(current_match, search_pattern[len(current_match):]) for current_match in current_matches])
+# while partial_matches:
+#     match_to_check = partial_matches.popleft()
+#     k, v = match_to_check
+#     next_matches = find_matches(v, towel_types)
+#     for next_match in next_matches:
+#         next_key = k + next_match
+#         if next_key == search_pattern:
+#             return search_pattern
+#         partial_matches.append((k + next_match, v[len(next_match):]))
+# return None
+
+@cache
+def run_matching(search_pattern: str, towel_types: frozenset[str]) -> int:
+    matches = 0
+    for towel in towel_types:
+        if search_pattern == towel:
+            matches += 1
         if search_pattern.startswith(towel):
-            yield towel
+            matches += run_matching(search_pattern[len(towel):], towel_types)
+    return matches
 
 
-def run_matching(search_pattern: str, towel_types: list[str]):
-    current_matches = find_matches(search_pattern, towel_types)
-    if current_matches is None:
-        return None
-    partial_matches = deque([(current_match, search_pattern[len(current_match):]) for current_match in current_matches])
-    while partial_matches:
-        match_to_check = partial_matches.popleft()
-        k, v = match_to_check
-        next_matches = find_matches(v, towel_types)
-        for next_match in next_matches:
-            next_key = k + next_match
-            if next_key == search_pattern:
-                return search_pattern
-            partial_matches.append((k + next_match, v[len(next_match):]))
-    return None
+def general_solve(input_segments: list[str], part_b: bool = False) -> int:
+    towel_types = [towel.strip() for towel in input_segments[0].split(',')]
+    patterns = input_segments[1].splitlines()
+
+    searched = {}
+    towels = frozenset(sorted(towel_types, key=lambda t: len(t), reverse=True))
+    for pattern_index in range(len(patterns)):
+        pattern = patterns[pattern_index]
+        searched[pattern] = run_matching(pattern, towels)
+    if part_b:
+        return sum([v for k, v in searched.items()])
+    return sum([1 if v > 0 else 0 for k, v in searched.items()])
 
 
 def part1_solve(input_segments: list[str]) -> int:
-    towel_types = [towel.strip() for towel in input_segments[0].split(',')]
-    searched = []
-
-    patterns = input_segments[1].splitlines()
-    for pattern_index in range(len(patterns)):
-        pattern = patterns[pattern_index]
-        searched.append(run_matching(pattern, towel_types))
-        ic(f'{pattern_index} of {len(patterns)} complete')
-    valid_patterns = len(list(filter(lambda x: x is not None, searched)))
-    return valid_patterns
+    return general_solve(input_segments)
 
 
-def part2_solve(input_data: str, grid_size: int) -> str:
-    return 0
+def part2_solve(input_segments: list[str]) -> int:
+    return general_solve(input_segments, True)
 
 
 def main() -> None:
@@ -81,11 +95,9 @@ def main() -> None:
 
     if int(example.answer_a) == ic(part1_solve(example_input)):
         puzzle.answer_a = ic(part1_solve(input_lines))
-    # if 22 == ic(part1_solve(example_input, 7, 12)):
-    #     puzzle.answer_a = ic(part1_solve(input_lines, 71, 1024))
 
-    # if "6,1" == ic(part2_solve(example_input, 7)):
-    #     puzzle.answer_b = ic(part2_solve(input_lines, 71))
+    if 16 == ic(part2_solve(example_input)):
+        puzzle.answer_b = ic(part2_solve(input_lines))
 
 
 if __name__ == '__main__':
